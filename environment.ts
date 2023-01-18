903e3fe0b5790466114101bf35ce0c209f235ce2
Antonio Pertuz <Antonio.Pertuz@focusservices.com>
2023-01-18 17:00:49 -0600

    chore: add enviroment variables

---
+++ environment.ts
@@ -0,0 +1,3 @@
+export const BIBLE_API_URL = 'https://api.scripture.api.bible/v1';
+export const BIBLE_API_KEY = '4cac45c125fab239b0e5f17cc82290b8';
+export const EVER_API_URL = 'https://iglesia-ever-app.herokuapp.com';
