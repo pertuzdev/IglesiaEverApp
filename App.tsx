0e5ba69ec0d36594770e2ef6927038b970712b95
Antonio Pertuz <Antonio.Pertuz@focusservices.com>
2023-05-29 16:59:45 -0600

    feat: implement notification subscriber

--- App.tsx
+++ App.tsx
@@ -8,10 +8,37 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigator } from './src/router/Root.stack';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    // Subscribe to a topic
+    const subscribeToTopic = async (topicName: any) => {
+      try {
+        await messaging().subscribeToTopic(topicName);
+        console.log(`Subscribed to topic: ${topicName}`);
+      } catch (error) {
+        console.error(`Error subscribing to topic: ${error}`);
+      }
+    };
+
+    // Call the function to subscribe to a topic
+    subscribeToTopic('allDevices-1501');
+
+    // Handle background notifications
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log('Background notification:', remoteMessage);
+      // Handle the received notification in the background
+    });
+
+    // Handle foreground notifications
+    messaging().onMessage((remoteMessage) => {
+      console.log('Foreground notification:', remoteMessage);
+      // Handle the received notification in the foreground
+    });
+  }, []);
   return <Navigator />;
 };
 
