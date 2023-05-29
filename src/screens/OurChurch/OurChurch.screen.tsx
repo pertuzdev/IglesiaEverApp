3545f7feff0d25cb962e27fa7196b0c4c328ade3
Antonio Pertuz <Antonio.Pertuz@focusservices.com>
2023-05-29 17:03:25 -0600

    chore: restore screen

--- src/screens/OurChurch/OurChurch.screen.tsx
+++ src/screens/OurChurch/OurChurch.screen.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { SafeAreaView, FlatList, Button, Text, View } from 'react-native';
+import { SafeAreaView, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SecondaryCard } from '../../components/SecondaryCard/SecondaryCard';
 import { Header } from '../../components/Header/Header';
 import { styles } from './OurChurch.styles';
-import notifee from '@notifee/react-native';
-import messaging from '@react-native-firebase/messaging';
 
 const churchOptions = [
   {
@@ -29,50 +27,20 @@ const churchOptions = [
 ];
 
 export const OurChurchScreen = () => {
-  async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission();
-
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    });
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }
-
-  const onMessageReceived = async (message: any) => {
-    console.log('entro');
-
-    console.log('message', message);
-  };
-
-  messaging().setBackgroundMessageHandler(onMessageReceived);
-
   const navigation = useNavigation<any>();
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <View>
-        <Text>AAAAHHHHHHHH</Text>
-      </View>
-
-      <Button
-        title="Display Notification"
-        color="red"
-        onPress={() => onDisplayNotification()}
+      <Header withAnimation={false} title="Nuestra Iglesia" />
+      <FlatList
+        style={styles.flatlist}
+        contentContainerStyle={styles.flatlistContent}
+        data={churchOptions}
+        renderItem={({ item }) => (
+          <SecondaryCard
+            onPress={() => navigation.navigate(item.navigateTo)}
+            title={item.title}
+          />
+        )}
       />
     </SafeAreaView>
   );
